363e3a546d2b4ad3990daa1218443048cfe03263
Janne Mäki <janne.maki@evident.fi>
2023-01-19 17:43:20 +0200

    Optional configuration for partial analyzing

---
+++ src/main/java/fi/evident/raudikko/AnalyzerConfiguration.java
@@ -0,0 +1,97 @@
+/*
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * https://www.mozilla.org/en-US/MPL/2.0/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Libvoikko: Library of natural language processing tools.
+ * The Initial Developer of the Original Code is Harri Pitkänen <hatapitk@iki.fi>.
+ * Portions created by the Initial Developer are Copyright (C) 2012
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Raudikko, the Java port of the Initial Code is Copyright (C) 2020 by
+ * Evident Solutions Oy. All Rights Reserved.
+ *
+ * Alternatively, the contents of this file may be used under the terms of
+ * either the GNU General Public License Version 2 or later (the "GPL"), or
+ * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+ * in which case the provisions of the GPL or the LGPL are applicable instead
+ * of those above. If you wish to allow use of your version of this file only
+ * under the terms of either the GPL or the LGPL, and not to allow others to
+ * use your version of this file under the terms of the MPL, indicate your
+ * decision by deleting the provisions above and replace them with the notice
+ * and other provisions required by the GPL or the LGPL. If you do not delete
+ * the provisions above, a recipient may use your version of this file under
+ * the terms of any one of the MPL, the GPL or the LGPL.
+ */
+
+package fi.evident.raudikko;
+
+/**
+ * Default configuration includes sensible defaults (most of the analysis excluding expensive).
+ * If this configuration is modified after constructing the analyzer the behaviour is undefined.
+ */
+public final class AnalyzerConfiguration {
+
+    private boolean includeStructure = true;
+    private boolean includeBaseForm = true;
+    private boolean includeBasicAttributes = true;
+    private boolean includeOrganizationNameAnalysis = true;
+    private boolean includeFstOutput = true;
+
+    public boolean isIncludeStructure() {
+        return includeStructure;
+    }
+
+    public void setIncludeStructure(boolean includeStructure) {
+        this.includeStructure = includeStructure;
+    }
+
+    public boolean isIncludeBaseForm() {
+        return includeBaseForm;
+    }
+
+    public void setIncludeBaseForm(boolean includeBaseForm) {
+        this.includeBaseForm = includeBaseForm;
+    }
+
+    public boolean isIncludeBasicAttributes() {
+        return includeBasicAttributes;
+    }
+
+    public void setIncludeBasicAttributes(boolean includeBasicAttributes) {
+        this.includeBasicAttributes = includeBasicAttributes;
+    }
+
+    public boolean isIncludeOrganizationNameAnalysis() {
+        return includeOrganizationNameAnalysis;
+    }
+
+    public void setIncludeOrganizationNameAnalysis(boolean includeOrganizationNameAnalysis) {
+        this.includeOrganizationNameAnalysis = includeOrganizationNameAnalysis;
+    }
+
+    public boolean isIncludeFstOutput() {
+        return includeFstOutput;
+    }
+
+    public void setIncludeFstOutput(boolean includeFstOutput) {
+        this.includeFstOutput = includeFstOutput;
+    }
+
+    @Override
+    public String toString() {
+        return "AnalyzerConfiguration{" +
+                "includeStructure=" + includeStructure +
+                ", includeBaseForm=" + includeBaseForm +
+                ", includeBasicAttributes=" + includeBasicAttributes +
+                ", includeOrganizationNameAnalysis=" + includeOrganizationNameAnalysis +
+                ", includeFstOutput=" + includeFstOutput +
+                '}';
+    }
+}
